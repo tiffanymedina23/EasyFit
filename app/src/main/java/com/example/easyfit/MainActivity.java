46823c622dc2b35b2b04119aff30673ae136dfb9
Tiffany Medina <t.medina1023@gmail.com>
2020-11-09 16:55:57 -0500

    Foundation of the EasyFit App

---
+++ app/src/main/java/com/example/easyfit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.easyfit;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
