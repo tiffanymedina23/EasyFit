7422ad7f49422b9fe81df839ab7ab8f3ecdf5a80
vanessamyron <vanessa.myron@yahoo.co.uk>
2020-11-13 20:39:33 -0500

    Splash Screen
    Images and animation for splash screen

--- app/src/main/java/com/example/easyfit/MainActivity.java
+++ app/src/main/java/com/example/easyfit/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.easyfit;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,10 +19,26 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+        //Variables
+        Animation topAnimation, bottomAnimation, slideLeft;
+        TextView sp_tv;
+        Button welcome;
+        ProgressBar loading;
+        ImageView log_in;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // FULLSCREEN MODE
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.slide_left);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        slideLeft =  AnimationUtils.loadAnimation(this,R.anim.slide_left);
     }
 
 
