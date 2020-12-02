2ab04ec4f7cdd91b7a60bc9f17d732d2f02a5a09
vanessamyron <vanessa.myron@yahoo.co.uk>
2020-12-02 11:01:50 -0500

    Timer
    Set the timer for workout

---
+++ app/src/main/java/com/example/easyfit/StartWorkoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.easyfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startworkout);
+    }
+}
