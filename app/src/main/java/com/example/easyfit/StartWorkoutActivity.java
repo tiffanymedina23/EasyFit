6078d15c2387bea28af83606a42b3f731c94381a
vanessamyron <vanessa.myron@yahoo.co.uk>
2020-12-02 12:43:00 -0500

    Countdown
    Timer Countdown

--- app/src/main/java/com/example/easyfit/StartWorkoutActivity.java
+++ app/src/main/java/com/example/easyfit/StartWorkoutActivity.java
@@ -3,12 +3,49 @@ package com.example.easyfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class StartWorkoutActivity extends AppCompatActivity {
+    TextView timerValue;
+    private static final long START_TIME_IN_MILLIS = 800000;
+    private CountDownTimer countDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startworkout);
+
+        timerValue = (TextView) findViewById(R.id.timerValue);
+    }
+
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Congratulations", Toast.LENGTH_SHORT).show();
+
+            }
+        } .start();
+        mTimerRunning = true;
+    }
+    
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeft = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerValue.setText(timeLeft);
     }
 }
